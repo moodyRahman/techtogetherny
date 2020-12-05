74842521497fef8d94dd2712900505e4cac4a3fe
Moody Rahman <mrahman01@stuy.edu>
2020-12-05 10:15:36 -0500

    salt field

--- api/db.py
+++ api/db.py
@@ -1,11 +1,12 @@
 
 from mongoengine import *
 
-connect(host="")
+connect(host="mongodb+srv://ttny:doof@moody.tjfji.mongodb.net/moody?retryWrites=true&w=majority")
 
 
 class User(Document):
     name = StringField()
     password = StringField()
+    salt = StringField()
 
 print("loaded db")
