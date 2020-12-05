c61c04d85103a0381cfd816f048d26fbdcab64e5
Moody Rahman <mrahman01@stuy.edu>
2020-12-05 16:47:55 -0500

    login logic

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { tokenContext } from "./Context.js"
 import "./App.scss";
 
 export default function Login() {
+  const { token, setToken } = useContext(tokenContext)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [redirect, setRedirect] = useState("")
+
+  const login = (event) => {
+    event.preventDefault();
+    const data = {
+      "username": username,
+      "password": password
+    }
+
+    fetch("http://moodyrahman.com:5000/auth", {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    })
+      .then(res => res.json())
+      .then(response => {
+        console.log(response)
+        setToken(response.token)
+      })
+
+  }
+
+  if (redirect) {
+    return (
+      <>
+      </>
+    )
+  }
+  else {
+
+  }
+
   return (
+
     <div className="login">
       <form>
-        <label>Which zombie are you? <input type="text" name="username" id="" /></label>
-        <label>How do we know it's you? <input type="password" name="password" id="" /></label>
-        <button type="submit">LOGIN</button>
+        <label>Which zombie are you? <input type="text" name="username" id="" onChange={e => setUsername(e.target.value)} /></label>
+        <label>How do we know it's you? <input type="password" name="password" id="" onChange={e => setPassword(e.target.value)} /></label>
+        <button type="submit" onClick={login}>LOGIN</button>
       </form>
     </div>
   );
